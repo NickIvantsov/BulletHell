ee2405df68921014ac6f59d76eed71fc843d2797
Nickolay-Ivantsov <ivantsov.nikolai.01@gmail.com>
2020-08-03 18:41:48 +0300

    инициализация проекта

---
+++ BulletHell/app/src/main/java/com/gmail/rewhelldev/BulletHellActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.rewhelldev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BulletHellActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
